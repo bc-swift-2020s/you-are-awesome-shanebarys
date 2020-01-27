8c1e89e5e51042cc5197da7ac6a45421c6be38b6
Shane Barys <shanejbarys@gmail.com>
2020-01-27 00:35:47 -0500

    2.14 Sounds added

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -16,6 +17,7 @@ class ViewController: UIViewController {
     var imageNumber = -1
     var messageNumber = -1
     let totalNumberOfImages = 9
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +45,18 @@ class ViewController: UIViewController {
     } while imageNumber == newImageNumber
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "Image\(imageNumber)")
+        
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                 print("🤬 ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object")
+            }
+
+        } else {
+            print("🤬 ERROR: Could not read data from file sound0")
+        }
 }
 }
 
